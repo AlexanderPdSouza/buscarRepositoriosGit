2eb993552d8a82a7e3da0cc6af34f9556c53437b
Alexander Souza <alexnada@gmail.com>
2022-03-24 22:12:46 -0300

    Primeira versão

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "bulma/css/bulma.min.css";
 
-function App() {
+function App() {''
+  const [user, setUser] = useState();
+  const [repository, setRepository] = useState([]);
+  const handleChange = (event) => setUser(event.target.value);
+
+  async function getRepository(user) {
+    const response = await fetch(`https://api.github.com/users/${user}/repos`);
+    const data = await response.json();
+    return data;
+  }
+  
+  function repositoryN() {
+    const liSemRepository = document.createElement("li");
+    liSemRepository.innerHTML = `Não existe repositorio para <b>${user}</b>`;
+    const elementPai = document.getElementById("ul-list");
+    elementPai.append(liSemRepository);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const resp = await getRepository(user);
+    if (resp.length > 0) {
+      return setRepository(resp);
+    } else {
+      repositoryN();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="user">User: </label>
+                <input
+                  className="input"
+                  name="user"
+                  type="text"
+                  value={user}
+                  onChange={handleChange}
+                ></input>
+                <br />
+                <input type="submit" className="button is-success"></input>
+              </div>
+            </form>
+          </div>
+          <div className="column">
+            <h1>{repository.length} repositorios</h1>
+            <ul id="ul-list">
+              {repository.map((repo) => (
+                <a href={repo.url} key={repo.url} id='url-list'>
+                  <li key={repo.name} url={repo.url}>
+                    {repo.name} {repo.stargazers_count}
+                  </li>
+                </a>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
